7d910896f4466657cfa3bf4c520765795af38197
Borja Esteban <boressan@outlook.com>
2023-05-20 07:40:17 +0000

    update items route

--- app/api/api_v1/items.py
+++ app/api/api_v1/items.py
@@ -1,4 +1,5 @@
-from typing import Any, List
+from typing import List
+from uuid import UUID
 
 from fastapi import APIRouter, Depends, HTTPException
 
@@ -7,53 +8,27 @@ from app import models, schemas
 router = APIRouter()
 
 
-@router.get("/", response_model=List[schemas.Item])
-def read_items(
-    # db: Session = Depends(deps.get_db),
-    skip: int = 0,
-    limit: int = 100,
-) -> Any:
-    """Retrieve items.
-    """
-    raise NotImplementedError
-
-
-@router.post("/", response_model=schemas.Item)
-def create_item(
-    # db: Session = Depends(deps.get_db),
-    item_in: schemas.ItemCreate,
-) -> Any:
-    """Create new item.
+@router.get("/{id}", response_model=List[schemas.Item])
+def read_items(id: UUID):
+    """Retrieve pool items.
     """
     raise NotImplementedError
 
 
 @router.put("/{id}", response_model=schemas.Item)
-def update_item(
-    # db: Session = Depends(deps.get_db),
-    id: int,
+def define_item(
+    id: UUID,
     item_in: schemas.ItemUpdate,
-) -> Any:
+):
     """Update an item.
-    """
-    raise NotImplementedError
-
-
-@router.get("/{id}", response_model=schemas.Item)
-def read_item(
-    # db: Session = Depends(deps.get_db),
-    id: int,
-) -> Any:
-    """Get item by ID.
     """
     raise NotImplementedError
 
 
 @router.delete("/{id}", response_model=schemas.Item)
 def delete_item(
-    # db: Session = Depends(deps.get_db),
-    id: int,
-) -> Any:
+    id: UUID,
+):
     """Delete an item.
     """
     raise NotImplementedError
