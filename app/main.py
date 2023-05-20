e7fe9fddae8fc4d23bda4a440b9890313bfa7e53
Borja Esteban <boressan@outlook.com>
2023-05-20 07:39:11 +0000

    update main

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 
 from app.api import api_v1
+from app.config import settings
 
-API_STR: str = f"/api/v1"
-app = FastAPI(title=__package__, openapi_url=f"{API_STR}/openapi.json")
-app.include_router(api_v1.router, prefix=API_STR)
+app = FastAPI(title=__package__, **settings.app.dict())
+app.include_router(api_v1.router, prefix=settings.v1_str)
