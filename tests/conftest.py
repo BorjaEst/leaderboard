425804f874baa0f3e48dc00b62d3645ed4e88506
Borja Esteban <boressan@outlook.com>
2023-05-24 11:58:14 +0000

    update tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,14 +2,8 @@
 # pylint: disable=redefined-outer-name
 # pylint: disable=unused-argument
 import pytest
-from leaderboard import Leaderboard
 
 
 @pytest.fixture(scope="session", param=["default", "config2"])
 def config(request):
     return f".tests/test_configurations/{request.param}.toml"
-
-
-@pytest.fixture(scope="session")
-def leaderboard(config):
-    yield Leaderboard('name', config_file=config)
